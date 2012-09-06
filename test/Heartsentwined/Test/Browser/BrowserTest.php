27334a1b744c16517bfcbdf133d4fe28cc8e04c9
heartsentwined <heartsentwined@cogito-lab.com>
2012-09-06 17:02:25 +0800

    added non existent URL tests

--- test/Heartsentwined/Test/Browser/BrowserTest.php
+++ test/Heartsentwined/Test/Browser/BrowserTest.php
@@ -32,6 +32,15 @@ class BrowserTest extends \PHPUnit_Framework_TestCase
     {
         $body = $this->browser->get('http://google.com');
         $this->assertNotEmpty($body);
+
+        $body = $this->browser->get('http://asdfasdfasdfasdfasdfasdf.com');
+        $this->assertSame('', $body);
+
+        $body = $this->browser->get('');
+        $this->assertSame('', $body);
+
+        $body = $this->browser->get('asdf');
+        $this->assertSame('', $body);
     }
 
     public function testIndirectGet()
@@ -39,12 +48,33 @@ class BrowserTest extends \PHPUnit_Framework_TestCase
         $this->browser->setUri('http://google.com');
         $body = $this->browser->get();
         $this->assertNotEmpty($body);
+
+        $this->browser->setUri('http://asdfasdfasdfasdfasdfasdf.com');
+        $body = $this->browser->get();
+        $this->assertSame('', $body);
+
+        $this->browser->setUri('');
+        $body = $this->browser->get();
+        $this->assertSame('', $body);
+
+        $this->browser->setUri('asdf');
+        $body = $this->browser->get();
+        $this->assertSame('', $body);
     }
 
     public function testPost()
     {
         $body = $this->browser->post('http://google.com', array());
         $this->assertNotEmpty($body);
+
+        $body = $this->browser->post('http://asdfasdfasdfasdfasdfasdf.com', array());
+        $this->assertSame('', $body);
+
+        $body = $this->browser->post('', array());
+        $this->assertSame('', $body);
+
+        $body = $this->browser->post('asdf', array());
+        $this->assertSame('', $body);
     }
 
     public function testIndirectPost()
@@ -52,5 +82,17 @@ class BrowserTest extends \PHPUnit_Framework_TestCase
         $this->browser->setUri('http://google.com');
         $body = $this->browser->post('', array());
         $this->assertNotEmpty($body);
+
+        $this->browser->setUri('http://asdfasdfasdfasdfasdfasdf.com');
+        $body = $this->browser->post('', array());
+        $this->assertSame('', $body);
+
+        $this->browser->setUri('');
+        $body = $this->browser->post('', array());
+        $this->assertSame('', $body);
+
+        $this->browser->setUri('asdf');
+        $body = $this->browser->post('', array());
+        $this->assertSame('', $body);
     }
 }
